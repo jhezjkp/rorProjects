d18e4115c3efd23545b7ca47cf0f2f093c6cd264
jhezjkp <jhezjkp@163.com>
2013-03-12 20:59:01 +0800

    forum demo project

---
+++ forum_demo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ForumDemo::Application.config.secret_token = 'faf8ce233ab57ce46cb50293a410b6161ea2d5ba2ca87bf297e0d18296ef4314d80b14104641a08e11c2e1cd7edc6763f9a9fdc4a7e51c32c54460ee80a8fa68'
