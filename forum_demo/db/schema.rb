43ee4d30fda2b3ffc5e783276182cc95e0682b91
jhezjkp <jhezjkp@163.com>
2013-03-12 21:34:23 +0800

    add board and post

--- forum_demo/db/schema.rb
+++ forum_demo/db/schema.rb
@@ -11,6 +11,19 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20130312133347) do
+
+  create_table "boards", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "posts", :force => true do |t|
+    t.string   "title"
+    t.string   "content"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
 end
