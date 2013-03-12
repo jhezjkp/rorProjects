65ef67cb21f63331e8bffe54c32e6000456613b3
jhezjkp <jhezjkp@163.com>
2013-03-12 21:40:36 +0800

    dd board_id to posts

--- forum_demo/app/models/post.rb
+++ forum_demo/app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ActiveRecord::Base
+  has_one :board
   attr_accessible :content, :title
 end
--- forum_demo/db/schema.rb
+++ forum_demo/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130312133347) do
+ActiveRecord::Schema.define(:version => 20130312133616) do
 
   create_table "boards", :force => true do |t|
     t.string   "name"
