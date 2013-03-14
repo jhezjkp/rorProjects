408ec7686ab0ba4ba7238405e6a69cae7cafb342
jhezjkp <jhezjkp@163.com>
2013-03-14 12:06:05 +0800

    customise ckeditor toolbar

---
+++ forum_demo/app/assets/javascripts/ckeditor/config.js
@@ -0,0 +1,20 @@
+CKEDITOR.editorConfig = function( config )
+{
+    config.language = 'zh-CN';  
+    config.toolbar = 'MyToolbar';
+
+    config.toolbar_MyToolbar =
+      [
+        { name: 'document', items : [ 'Source','Preview' ] },
+        { name: 'clipboard', items : [ 'Cut','Copy','Paste','PasteText','PasteFromWord','-','Undo','Redo' ] },
+        { name: 'editing', items : [ 'Find','Replace','-','SelectAll','-','Scayt' ] },
+        { name: 'insert', items : [ 'Image','Flash','Table','HorizontalRule','Smiley','SpecialChar','PageBreak'
+               ,'Iframe' ] },
+        '/',
+        { name: 'styles', items : [ 'Styles','Format' ]     },
+        { name: 'basicstyles', items : [ 'Bold','Italic','Strike','-','RemoveFormat' ] },
+        { name: 'paragraph', items : [ 'NumberedList','BulletedList','-','Outdent','Indent','-','Blockquote' ] },
+        { name: 'links', items : [ 'Link','Unlink','Anchor' ] },
+        { name: 'tools', items : [ 'Maximize','-','About' ] }
+    ];
+};
