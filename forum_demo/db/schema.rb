dfa0028f8c8a37d27d0d05f194535e376dabbd5c
jhezjkp <jhezjkp@163.com>
2013-03-14 11:07:55 +0800

    intergrate ckeditor

--- forum_demo/db/schema.rb
+++ forum_demo/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130313134940) do
+ActiveRecord::Schema.define(:version => 20130314104626) do
 
   create_table "boards", :force => true do |t|
     t.string   "name"
@@ -19,6 +19,22 @@ ActiveRecord::Schema.define(:version => 20130313134940) do
     t.datetime "updated_at",                 :null => false
     t.integer  "posts_count", :default => 0
   end
+
+  create_table "ckeditor_assets", :force => true do |t|
+    t.string   "data_file_name",                  :null => false
+    t.string   "data_content_type"
+    t.integer  "data_file_size"
+    t.integer  "assetable_id"
+    t.string   "assetable_type",    :limit => 30
+    t.string   "type",              :limit => 30
+    t.integer  "width"
+    t.integer  "height"
+    t.datetime "created_at",                      :null => false
+    t.datetime "updated_at",                      :null => false
+  end
+
+  add_index "ckeditor_assets", ["assetable_type", "assetable_id"], :name => "idx_ckeditor_assetable"
+  add_index "ckeditor_assets", ["assetable_type", "type", "assetable_id"], :name => "idx_ckeditor_assetable_type"
 
   create_table "posts", :force => true do |t|
     t.string   "title"
