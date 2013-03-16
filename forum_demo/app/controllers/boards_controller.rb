741bb828bf8e75a63fc8d3978218100242d3f20d
jhezjkp <jhezjkp@163.com>
2013-03-16 17:49:22 +0800

    ui update

--- forum_demo/app/controllers/boards_controller.rb
+++ forum_demo/app/controllers/boards_controller.rb
@@ -2,8 +2,8 @@ class BoardsController < ApplicationController
   # GET /boards
   # GET /boards.json
   def index
-    @boards = Board.all
-
+    @posts = Post.recent.paginate(:page => params[:page], :per_page => 10)
+    
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @boards }
