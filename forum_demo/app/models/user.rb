65473f1a3e41b4f613a8d7f096f3cc92fc8f641c
root <root@debian.(none)>
2013-03-18 17:08:02 +0800

    intergrate github

--- forum_demo/app/models/user.rb
+++ forum_demo/app/models/user.rb
@@ -19,6 +19,38 @@ class User < ActiveRecord::Base
   attr_protected :is_admin
 
   validates :name, :presence => true
+
+  def self.from_omniauth(auth)
+    where("email = ?", auth.info.email).first_or_create do |user|
+      user.email = auth.info.email
+      user.name = auth.info.name
+      user.provider = auth.provider
+      user.uid = auth.uid
+    end
+  end
+
+  def new_with_session(params, session)
+    if session["devise.user_attributes"]
+      new(session["devise.user_attributes"], without_protection: true) do |user|
+        user.attributes = params
+        user.valid?
+      end
+    else
+      super
+    end
+  end
+
+  def password_required?
+    super && provider.blank?
+  end
+
+  def update_with_password(params, *options)
+    if encrypted_password.blank?
+      update_attributes(params, *options)
+    else
+      super
+    end
+  end
   
   def is_admin?
     is_admin
--- forum_demo/db/schema.rb
+++ forum_demo/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130317132607) do
+ActiveRecord::Schema.define(:version => 20130318083608) do
 
   create_table "boards", :force => true do |t|
     t.string   "name"
@@ -60,6 +60,8 @@ ActiveRecord::Schema.define(:version => 20130317132607) do
     t.datetime "updated_at",                                :null => false
     t.boolean  "is_admin",               :default => false
     t.string   "name"
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
